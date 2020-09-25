4298d9f5577994c1848c32af649493d79172de94
winnie <463588826@qq.com>
2020-09-25 22:29:03 +0800

    This a GitTest

--- src/com/vge/GitTest.java
+++ src/com/vge/GitTest.java
@@ -4,6 +4,7 @@ package com.vge;
  * Created by vge on 2020/9/25
  */
 public class GitTest {
+    private String aa;
     public static void main(String[] args) {
         System.out.println("Hello GitHub!");
         System.out.println("Hello Vgehpu!");
