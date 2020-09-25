1c814411ebebd002e5255c49b0c3ac4908b3b597
winnie <463588826@qq.com>
2020-09-25 22:06:52 +0800

    This a GitTest

---
+++ src/com/vge/GitTest.java
@@ -0,0 +1,10 @@
+package com.vge;
+
+/**
+ * Created by vge on 2020/9/25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub!");
+    }
+}
