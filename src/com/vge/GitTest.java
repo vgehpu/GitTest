4e064348177a855b488dfe0daa6dfcafb79a79e5
winnie <463588826@qq.com>
2020-09-25 22:25:25 +0800

    This a gitTest vgehpu2006

--- src/com/vge/GitTest.java
+++ src/com/vge/GitTest.java
@@ -6,5 +6,6 @@ package com.vge;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello GitHub!");
+        System.out.println("Hello Vgehpu!");
     }
 }
